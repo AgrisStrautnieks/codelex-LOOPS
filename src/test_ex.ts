6cf864a092af38ff50e0a758d0ae6782fcd5263c
Agris <agrisstrautnieks@Agriss-MacBook-Pro.local>
2021-03-08 01:02:28 +0200

    first commit

---
+++ src/test_ex.ts
@@ -0,0 +1,17 @@
+
+
+import * as prompts from "prompts";
+
+(async () => {
+  const response = await prompts({
+    type: "text",
+    name: "num",
+    message: ">Enter the number!",
+  });
+   console.log(response.num);
+
+
+
+
+
+})();
